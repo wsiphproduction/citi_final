a12caa7101fe3931f44cf9d186f1672d82769f33
Splakatutay <ararborinaga@gmail.com>
2022-04-12 11:44:54 +0800

    change remove to disapproved

--- resources/views/pages/pcfr/payable/show.blade.php
+++ resources/views/pages/pcfr/payable/show.blade.php
@@ -143,7 +143,7 @@
                                 <td> 
 
                                     <nav class="nav table-options justify-content-start">
-                                        <a class="nav-link p-0 pl-2 remove_attachment" href="javascript:void(0);" title="Remove" data-target="#pcvRemove{{$pcv->id}}" data-toggle="modal" data-dismiss="modal">
+                                        <a class="nav-link p-0 pl-2 remove_attachment" href="javascript:void(0);" title="Disapproved" data-target="#pcvRemove{{$pcv->id}}" data-toggle="modal" data-dismiss="modal">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
                                         </a>
                                     </nav>
