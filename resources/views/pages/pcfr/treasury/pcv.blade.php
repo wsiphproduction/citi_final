d267b5ee639a461479165a3edaeaa490f844b17d
Splakatutay <ararborinaga@gmail.com>
2022-04-26 15:00:36 +0800

    04/26/2022 updates

--- resources/views/pages/pcfr/treasury/pcv.blade.php
+++ resources/views/pages/pcfr/treasury/pcv.blade.php
@@ -58,7 +58,7 @@
                             </td>
                             <td>
                             	<nav class="nav table-options">
-	                                <a class="nav-link p-0 pl-2" href="{{ rout('treasury.pcfr.show-pcv', $pcv->id) }}" title="View Request">
+	                                <a class="nav-link p-0 pl-2" href="{{ route('treasury.pcfr.show-pcv', $pcv->id) }}" title="View Request">
 	                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-eye"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
 	                                </a>
 	                            </nav>
